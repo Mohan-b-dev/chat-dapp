7d96d96b477aa2555a7ab6fdaca9ebcc232efc77
mohanmohan515 <mohangokul515@gmail.com>
2025-07-27 18:35:30 +0530

    ui for navbar

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,128 +1,110 @@
 import React, { useContext, useState } from "react";
-
-//INTERNAL IMPORT
 import Style from "./NavBar.module.css";
 import { ChatAppContext } from "../../Context/ChatAppContext";
-import { Model, Error } from "../index";
+import { Model } from "../index";
 import images from "../../assets";
 import Link from "next/link";
 import Image from "next/image";
 
 const NavBar = () => {
   const menuItems = [
-    {
-      menu: "All users",
-      link: "/alluser",
-    },
-    {
-      menu: "CHAT",
-      link: "/",
-    },
-    {
-      menu: "CONTACT",
-      link: "/",
-    },
-    {
-      menu: "SETTING",
-      link: "/",
-    },
-    {
-      menu: "FAQS",
-      link: "/",
-    },
-    {
-      menu: "TERMS OF USES",
-      link: "/",
-    },
+    { menu: "All users", link: "alluser" },
+    { menu: "CHAT", link: "/" },
+    { menu: "CONTACT", link: "/" },
+    { menu: "SETTING", link: "/" },
+    { menu: "FAQS", link: "/" },
+    { menu: "TERMS OF USES", link: "/" },
   ];
 
-  //USESTATE
   const [active, setActive] = useState(2);
   const [open, setOpen] = useState(false);
   const [openModel, setOpenModel] = useState(false);
 
   const { account, userName, connectWallet } = useContext(ChatAppContext);
+
   return (
-    <div className={Style.NavBar}>
+    <nav className={Style.NavBar} aria-label="Main navigation">
       <div className={Style.NavBar_Box}>
         <div className={Style.NavBar_Box_left}>
-          <Image src={images.logo} alt="logo" width={50} height={50} />
+          <Image src={images.logo} alt="Logo" width={50} height={50} priority />
         </div>
         <div className={Style.NavBar_Box_right}>
-          {/*DESKTOP */}
           <div className={Style.NavBar_Box_right_menu}>
             {menuItems.map((el, i) => (
               <div
                 onClick={() => setActive(i + 1)}
-                key={i}
+                key={i + 1}
                 className={`${Style.NavBar_Box_right_menu_items} ${
-                  active == i + 1 ? Style.active_btn : ""
+                  active === i + 1 ? Style.active_btn : ""
                 }`}
               >
-                <Link className={Style.NavBar_Box_right_menu} href={el.link}>
+                <Link
+                  className={Style.NavBar_Box_right_menu}
+                  href={el.link}
+                  aria-current={active === i + 1 ? "page" : undefined}
+                >
                   {el.menu}
                 </Link>
               </div>
             ))}
           </div>
-
-          {/*MOBILE */}
-          {open && (
-            <div className={Style.mobile_menu}>
-              {menuItems.map((el, i) => (
-                <div
-                  onClick={() => setActive(i + 1)}
-                  key={i}
-                  className={`${Style.mobile_menu_items} ${
-                    active == i + 1 ? Style.active_btn : ""
-                  }`}
-                >
-                  <Link className={Style.mobile_menu_items_link} href={el.link}>
-                    {el.menu}
-                  </Link>
-                </div>
-              ))}
-              <p className={Style.mobile_menu_btn}>
-                <Image
-                  src={images.close}
-                  alt="close"
-                  width={50}
-                  height={50}
-                  onClick={() => setOpen(false)}
-                />
-              </p>
-            </div>
-          )}
-
-          {/*CONNECT WALLET */}
-          <p>hi</p>
+          <div className={`${Style.mobile_menu} ${open ? Style.open : ""}`}>
+            {menuItems.map((el, i) => (
+              <div
+                onClick={() => {
+                  setActive(i + 1);
+                  setOpen(false);
+                }}
+                key={i}
+                className={`${Style.mobile_menu_items} ${
+                  active === i + 1 ? Style.active_btn : ""
+                }`}
+              >
+                <Link className={Style.mobile_menu_items} href={el.link}>
+                  {el.menu}
+                </Link>
+              </div>
+            ))}
+            <p className={Style.mobile_menu_btn}>
+              <Image
+                src={images.close}
+                alt="Close menu"
+                width={50}
+                height={50}
+                onClick={() => setOpen(false)}
+              />
+            </p>
+          </div>
           <div className={Style.NavBar_Box_right_connect}>
-            {account == "" ? (
+            {account === "" ? (
               <button onClick={() => connectWallet()}>
-                {""}
                 <span>Connect Wallet</span>
               </button>
             ) : (
               <button onClick={() => setOpenModel(true)}>
-                {""}
                 <Image
                   src={userName ? images.accountName : images.create2}
                   alt="Account image"
-                    width={20}
-                    height={20}
-                  />
-                  {""}
-                  <small>{userName||"create Account" }</small>
+                  width={20}
+                  height={20}
+                />
+                <small>{userName || "Create Account"}</small>
               </button>
             )}
           </div>
-
-          <div className={Style.NavBar_Box_right_open} onClick={() => setOpen(true)}>
-            <Image src={images.open} alt="open" width={30} height={30} />
+          <div
+            className={Style.NavBar_box_right_open}
+            onClick={() => setOpen(true)}
+            role="button"
+            aria-label="Open menu"
+            aria-expanded={open}
+          >
+            <Image src={images.open} alt="Open menu" width={30} height={30} />
           </div>
         </div>
       </div>
-    </div>
+      {openModel && <Model setOpenModel={setOpenModel} />}
+    </nav>
   );
 };
 
